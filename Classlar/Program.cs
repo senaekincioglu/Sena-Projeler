8e221185a61e39565fba99edd462eab6fb1c3d54
senaekincioglu <115897687+senaekincioglu@users.noreply.github.com>
2022-11-03 11:06:22 +0300

    1

---
+++ Classlar/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classlar
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //erişim türleri
+            //public: tüm classlarda çağrılıp, kullanılabilir.
+            //global: bir metot diye düşünülebilir.
+            //private: sadece bulunduğu class da çağrılır.
+            //static: bulunduğu class da metot adı ile direkt çağrılır
+            //public-static : genel bir değişken tanımlaması olmasına rağmen başka classa gidemez sadece kendi classında class nesnesi tanımlanmadan kullanılabilir.
+            //private-static: private clas özel tanımlama ve static ise classa ait değişken olduğundan dışarıdan çağrıldığında erişilemez.
+        }
+    }
+}
